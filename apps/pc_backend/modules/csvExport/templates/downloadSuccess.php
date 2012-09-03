6bd47a8bd86f77c287cb5fd136bbb59b05057268
watanabe <watanabe@openpne.jp>
2012-09-04 01:58:58 +0900

    fixed for i18n

--- apps/pc_backend/modules/csvExport/templates/downloadSuccess.php
+++ apps/pc_backend/modules/csvExport/templates/downloadSuccess.php
@@ -1,7 +1,7 @@
-<h2>メンバーCSV ダウンロード</h2>
+<h2><?php echo __('Member CSV download') ?></h2>
 <form action="<?php echo url_for('@csvExport_download') ?>" method="POST">
 <table>
 <?php echo $form ?>
 </table>
-<input type="submit" value="ダウンロード" />
+<input type="submit" value="<?php echo __('Download') ?>" />
 </form>
